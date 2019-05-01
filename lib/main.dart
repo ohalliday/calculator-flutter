8f8f647bc32e692dfae592c6bca7b26e81f99d29
Owen Halliday <owenhalliday1@gmail.com>
2019-05-01 13:11:12 +0100

    Added button functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,73 @@ class _MyHomePageState extends State<MyHomePage> {
   
   String output = "0"; 
 
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed (String buttonText) {
+    if (buttonText == "CLEAR") {
+
+        String _output = "0";
+        double num1 = 0.0;
+        double num2 = 0.0;
+        String operand = "";
+
+    } else if (buttonText == "+" || buttonText == "-" || buttonText == "/" || buttonText == "X") {
+      
+      num1 = double.parse(output);
+
+      operand = buttonText;
+
+      _output = "0";
+
+    } else if (buttonText == ".") {
+
+      if(_output.contains(".")) {
+        print("Already contains a decimal!");
+        return;
+      } else {
+      _output = _output + buttonText;
+      } 
+
+    } else if (buttonText == "=") {
+
+    num2 = double.parse(output);
+
+    if (operand == "+") {
+      _output = (num1 + num2).toString();
+    }
+
+    if (operand == "-") {
+      _output = (num1 + num2).toString();
+    }
+
+    if (operand == "X") {
+      _output = (num1 * num2).toString();
+    }
+
+    if (operand == "/") {
+      _output = (num1 / num2).toString();
+    }
+
+    num1 = 0.0;
+    num2 = 0.0;
+    _output = "0";
+
+  } else {
+
+    _output = _output + buttonText;
+
+  }
+
+  print(_output);
+
+  setState(() {
+    output = double.parse(_output).toStringAsFixed(2);
+  }); 
+} 
+
   Widget newButton(String buttonText) {
     return Expanded(
         child: new OutlineButton(
@@ -38,7 +105,9 @@ class _MyHomePageState extends State<MyHomePage> {
               fontWeight: FontWeight.bold,
             ),
             ),
-          onPressed: () => {},
+          onPressed: () => 
+            buttonPressed(buttonText)
+          ,
           color: Colors.lightBlue,
           textColor: Colors.blue,
     ),
